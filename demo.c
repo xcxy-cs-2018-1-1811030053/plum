2726b6d003a027a12c0dc7ae4d56970f2253de9f
三寸人间 <321293286@qq.com>
2020-11-30 17:06:54 +0800

    添加了一个c语言程序

---
+++ demo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+    int input = 0;
+
+    printf("input an interger:\n");
+
+    scanf("%d",&input);
+
+    printf("the interger you input is%d\n",input);
+
+    return 0;
+
+}
